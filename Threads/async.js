a1dd4be6d7c4e1bfea48434c965ea40d92e1dbf1
Dion <dion.boles@gmail.com>
2020-01-08 15:46:51 -0500

    added folder for Threads and async file

--- Threads/async.js
+++ Threads/async.js
@@ -1,10 +1,20 @@
 const https = require("https");
 const start = Date.now();
 
-// this makes a request to googles home page
-https.request("https://www.google.com", res =>{
-    res.on("data", () => {});
-    res.on("end",()=>{
-        console.log(Date.now() - start);
-    })
-}).end();
+let doRequest = () =>{
+    // this makes a request to googles home page
+    https.request("https://www.google.com", res =>{
+        res.on("data", () => {});
+        res.on("end",()=>{
+            console.log(Date.now() - start);
+        })
+    }).end();
+};
+
+doRequest();
+doRequest();
+doRequest();
+doRequest();
+doRequest();
+doRequest();
+doRequest();
